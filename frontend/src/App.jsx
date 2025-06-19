63e2a2875d839f78fe895010901ee42011a84b15
Shi Hui <shihui.khoo00@gmail.com>
2025-06-19 15:56:56 +0800

    embed assistant to app
    embed assistant to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -8,6 +8,36 @@ import Lumenh from "./pages/Lumenh";
 import ABCResidence from "./pages/ABCResidence";
 
 function App() {
+  useEffect(() => {
+    window.watsonAssistantChatOptions = {
+      integrationID: "e4b87da5-1d16-4c8f-98bc-c1c2c0d74bb5",
+      region: "us-south",
+      serviceInstanceID: "8647c42c-c307-4270-91ad-ed481a325879",
+      onLoad: async (instance) => {
+        await instance.render();
+
+        const style = document.createElement('style');
+        style.innerHTML = `
+          .WACWidget {
+            width: 35% !important;
+            height: 85% !important;
+          }
+          .WatsonAssistantChatHost {
+            margin: 0px;
+          }
+        `;
+        document.head.appendChild(style);
+      },
+    };
+
+    const script = document.createElement("script");
+    script.src = `https://web-chat.global.assistant.watson.appdomain.cloud/versions/${
+      window.watsonAssistantChatOptions.clientVersion || "latest"
+    }/WatsonAssistantChatEntry.js`;
+    script.async = true;
+    document.head.appendChild(script);
+  }, []);
+
   return (
     <Router>
       <Header />
