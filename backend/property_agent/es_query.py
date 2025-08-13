c842b99019cb19ce49de465e087c40dbf70fc844
liawyihui <yi.hui.liaw@ibm.com>
2025-08-13 17:19:33 +0800

    edited credentials

--- backend/property_agent/es_query.py
+++ backend/property_agent/es_query.py
@@ -2,21 +2,13 @@ import os
 import time
 from datetime import datetime, timedelta
 
-from elasticsearch import Elasticsearch, helpers
-from elasticsearch.helpers import bulk
+from elasticsearch import Elasticsearch
 from dotenv import load_dotenv
-from ibm_watsonx_ai import APIClient
-from ibm_watsonx_ai.foundation_models import ModelInference
-from ibm_watsonx_ai.foundation_models.prompts import PromptTemplate, PromptTemplateManager
-from ibm_watsonx_ai.metanames import GenTextParamsMetaNames as GenParams
-from llama_index.core.node_parser import SentenceSplitter
-import json
-from elasticsearch.helpers import BulkIndexError
 load_dotenv(override=True)
 
-def connect_es():
-    es_url = "http://itzvsi-693000gwtq-wxd-1xy2tsbq.techzone.ibm.com:9200/"
-    es = Elasticsearch(hosts=es_url, basic_auth=ES_CREDENTIALS, verify_certs=False, ssl_show_warn=False, request_timeout=600)
+def connect_es(es_url, es_credentials):
+    es_url = os.getenv()
+    es = Elasticsearch(hosts=es_url, basic_auth=es_credentials, verify_certs=False, ssl_show_warn=False, request_timeout=600)
     print(es.info())
     return es
 
@@ -53,7 +45,8 @@ if __name__ == "__main__":
     PIPELINE_ID = "ingest-pipeline-property"
     WML_CREDENTIALS = {"apikey": os.getenv("WATSONX_API_KEY"), "url": "https://us-south.ml.cloud.ibm.com"}
     ES_CREDENTIALS = (os.getenv("ES_USERNAME"), os.getenv("ES_PASSWORD"))
+    ES_URL = os.getenv("ES_URL")
     model_name = ".elser_model_2"
 
-    es = connect_es()
+    es = connect_es(ES_URL, ES_CREDENTIALS)
     es_query(es, INDEX_NAME, "Properties in Mont Kiara with price 500k", model_name)
